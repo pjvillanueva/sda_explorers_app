feed4c795a9b3a0460d98cf2c43a0f4f4b889217
Paul James Villanueva <77760810+pjvillanueva@users.noreply.github.com>
2025-02-09 09:47:52 +0800

    constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,30 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:flutter/material.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_1.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_10.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_11.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_12.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_13.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_14.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_15.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_16.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_17.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_18.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_19.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_2.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_20.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_21.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_22.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_23.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_24.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_3.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_4.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_5.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_6.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_7.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_8.dart';
+import 'package:sda_explorers_app/data/lessons/lesson_9.dart';
 
 const TextTheme TEXT_THEME_DEFAULT = TextTheme(
     // headline1: TextStyle(fontWeight: FontWeight.w700, fontSize: 26),
@@ -31,3 +55,37 @@ const TextTheme TEXT_THEME_SMALL = TextTheme(
     // subtitle1: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
     // subtitle2: TextStyle(fontSize: 10, fontWeight: FontWeight.w400)
     );
+
+const darkTextColor = Color(0xFF00072D);
+
+const OWNER_ROLE_ID = 'NRz4AqYcUzKolqIlxufC';
+const ADMIN_ROLE_ID = 'PxJq2zsWVs6sEqKdVdzJ';
+const GUIDE_ROLE_ID = '4M7IOwCOy4SEfWpJNyOC';
+const EXPLORER_ROLE_ID = 'QbOMk6MwGq8MTvxB0rNi';
+
+  final lessons = [
+    Lesson1ContentsMap,
+    Lesson2ContentsMap,
+    Lesson3ContentsMap,
+    Lesson4ContentsMap,
+    Lesson5ContentsMap,
+    Lesson6ContentsMap,
+    Lesson7ContentsMap,
+    Lesson8ContentsMap,
+    Lesson9ContentsMap,
+    Lesson10ContentsMap,
+    Lesson11ContentsMap,
+    Lesson12ContentsMap,
+    Lesson13ContentsMap,
+    Lesson14ContentsMap,
+    Lesson15ContentsMap,
+    Lesson16ContentsMap,
+    Lesson17ContentsMap,
+    Lesson18ContentsMap,
+    Lesson19ContentsMap,
+    Lesson20ContentsMap,
+    Lesson21ContentsMap,
+    Lesson22ContentsMap,
+    Lesson23ContentsMap,
+    Lesson24ContentsMap
+  ];

7ba1d5bef46a71ff1c120a9a206e290e959e573f
Paul James Villanueva <77760810+pjvillanueva@users.noreply.github.com>
2025-02-09 09:45:13 +0800

    test and user cubit

---
+++ lib/logic/cubits/test_cubit.dart
@@ -0,0 +1,69 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TestState extends Equatable {
+  final int currentQuestion;
+  final int totalQuestions;
+  final Map<String, dynamic> answers;
+
+  const TestState(
+      {required this.currentQuestion,
+      required this.totalQuestions,
+      required this.answers});
+
+  @override
+  List<Object> get props => [currentQuestion, totalQuestions, answers];
+
+
+  get isLastQuestion {
+    return currentQuestion == totalQuestions - 1;
+  }
+
+  TestState copyWith({
+    int? currentQuestion,
+    int? totalQuestions,
+    Map<String, dynamic>? answers,
+  }) {
+    return TestState(
+      currentQuestion: currentQuestion ?? this.currentQuestion,
+      totalQuestions: totalQuestions ?? this.totalQuestions,
+      answers: answers ?? this.answers,
+    );
+  }
+}
+
+class TestCubit extends Cubit<TestState> {
+  TestCubit()
+      : super(const TestState(
+            currentQuestion: 0, totalQuestions: 0, answers: {}));
+
+  void setInitialValue(int totalQuestions) {
+    emit(TestState(
+        currentQuestion: 0, totalQuestions: totalQuestions, answers: const {}));
+  }
+
+  back() {
+    if (state.currentQuestion > 0) {
+      emit(state.copyWith(currentQuestion: state.currentQuestion - 1));
+    }
+    print('BACK CLICKED');
+  }
+
+  void submit(String questionId, dynamic answer) {
+    //save answer
+    Map<String, dynamic> newAnswers = Map.from(state.answers);
+    newAnswers[questionId] = answer;
+    emit(state.copyWith(answers: newAnswers));
+    //submit or proceed to next question
+    if (state.currentQuestion < state.totalQuestions - 1) {
+      emit(state.copyWith(currentQuestion: state.currentQuestion + 1));
+    } else {
+      print('Submit all answers: ${state.answers}');
+    }
+    print('SUBMIT: Answers: ${state.answers}');
+  }
+
+  dynamic loadAnswer(String questionId) {
+    return state.answers[questionId];
+  }
+}
--- lib/logic/cubits/user_cubit.dart
+++ lib/logic/cubits/user_cubit.dart
@@ -1,32 +1,52 @@
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:sda_explorers_app/data/models/user.dart';
+import 'package:sda_explorers_app/data/models/user_roles.dart';
 
 class UserState {
-  UserState({required this.user});
+  UserState({required this.user, required this.role});
   final User? user;
+  final UserRole role;
 
-  Map<String, dynamic> toJson() => {'user': user?.toJson()};
+  Map<String, dynamic> toJson() =>
+      {'user': user?.toJson(), 'role': role.toJson()};
 
-  UserState.fromJson(Map<String, dynamic> json)
-      : user = User.fromJson(json['user']);
+  factory UserState.fromJson(Map<String, dynamic> json) {
+    return UserState(
+      user: json['user'] != null ? User.fromJson(json['user']) : null,
+      role: UserRole.fromJson(json['role']),
+    );
+  }
+
+  UserState copyWith({User? user, UserRole? role}) {
+    return UserState(
+      user: user ?? this.user,
+      role: role ?? this.role,
+    );
+  }
 
   @override
-  String toString() => 'user: $user';
+  String toString() => 'user: $user role: $role';
 
   User? get getUser => user;
+  UserRole get getRole => role;
 }
 
 class UserCubit extends HydratedCubit<UserState> {
-  UserCubit() : super(UserState(user: null));
+  UserCubit() : super(UserState(user: null, role: guestUserRole));
 
   // Update the user state
   void updateUser(User user) {
-    emit(UserState(user: user));
+    emit(state.copyWith(user: user));
+  }
+
+  //Update user role
+  void updateRole(UserRole role) {
+    emit(state.copyWith(role: role));
   }
 
   // Clear the user state
   void clearUser() {
-    emit(UserState(user: null));
+    emit(state.copyWith(user: null, role: guestUserRole));
   }
 
   @override

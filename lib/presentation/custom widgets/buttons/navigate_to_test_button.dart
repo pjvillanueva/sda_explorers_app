1b1ed5290cc7f0c8833a95499ebe683452f5641a
Paul James Villanueva <77760810+pjvillanueva@users.noreply.github.com>
2025-02-09 09:44:37 +0800

    navigate to test btn

--- lib/presentation/custom widgets/buttons/navigate_to_test_button.dart
+++ lib/presentation/custom widgets/buttons/navigate_to_test_button.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:sda_explorers_app/presentation/screens/tests%20screens/lesson_1_test_screen.dart';
+import 'package:sda_explorers_app/data/tests/test_1.dart';
+import 'package:sda_explorers_app/presentation/screens/Test/test_screen.dart';
 
 class NavigateToTest extends StatelessWidget {
-  const NavigateToTest(
-      {super.key, required this.testNumber, required this.testContents});
+  const NavigateToTest({super.key, required this.testNumber});
 
   final int testNumber;
-  final Map<String, String> testContents;
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +13,30 @@ class NavigateToTest extends StatelessWidget {
       style: ButtonStyle(backgroundColor: WidgetStateProperty.all(Colors.blue)),
       onPressed: () {
         Navigator.push(
-            context, MaterialPageRoute(builder: (context) => const Lesson1TestScreen()));
+            context,
+            MaterialPageRoute(
+                builder: (context) => TestScreen(
+                      title: 'Lesson $testNumber Test',
+                      questions: test1Questions,
+                    )));
       },
       child: const Text(
         'ANSWER LESSON TEST',
         style: TextStyle(color: Colors.white),
       ),
     );
+  }
+}
+
+//TODO: Solve bugs
+//Choices type select the same answer
+//Something went wrong in the last two fill blanks question
+
+_getQuestions(int testNumber) {
+  switch (testNumber) {
+    case 1:
+      return test1Questions;
+    default:
+      return test1Questions;
   }
 }

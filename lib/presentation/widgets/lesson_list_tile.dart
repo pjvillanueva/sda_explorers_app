d16a7a741dc7173b9726b37a74d5aac73abd0467
Paul James Villanueva <77760810+pjvillanueva@users.noreply.github.com>
2025-05-07 05:34:10 +0800

    feat: improve lesson list tile

--- lib/presentation/widgets/lesson_list_tile.dart
+++ lib/presentation/widgets/lesson_list_tile.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:sda_explorers_app/data/lessons/helpers.dart';
+import 'package:sda_explorers_app/presentation/custom%20widgets/snackbar.dart';
 import '../screens/lesson_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LessonListTile extends StatelessWidget {
-  const LessonListTile({super.key, required this.index, required this.content});
+  const LessonListTile(
+      {super.key,
+      required this.index,
+      required this.content,
+      this.isAllowed = false});
 
   final int index;
   final Map<String, String> content;
+  final bool isAllowed;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.only(bottom: 15),
       elevation: 2,
+      color: isAllowed ? null : Colors.grey[700],
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: ListTile(
         contentPadding:
             const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         leading: CircleAvatar(
-          backgroundColor: Colors.blue[700],
-          child: Text(
+          backgroundColor: isAllowed ? Colors.blue[700] : Colors.grey,
+          child: isAllowed? Text(
             intToRoman(index + 1),
             style: const TextStyle(
                 color: Colors.white, fontWeight: FontWeight.bold, fontSize: 13),
-          ),
+          ) :  const Icon(Icons.lock, color: Colors.white, size: 16),
         ),
         title: Text(
           '${AppLocalizations.of(context)!.homeLesson} ${index + 1}',
@@ -32,17 +39,23 @@ class LessonListTile extends StatelessWidget {
         ),
         subtitle: Text(
           content.values.first,
-          style: TextStyle(color: Colors.grey[600]),
+          style: TextStyle(color: isAllowed? Colors.grey[600] : Colors.grey[800]),
         ),
-        trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-        onTap: () {
-          // Navigate to lesson screen
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => LessonScreen(
-                      lessonContents: content, lessonNumber: index + 1)));
-        },
+        trailing: isAllowed ? const Icon(Icons.arrow_forward_ios, size: 16) : null,
+        onTap: isAllowed
+            ? () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => LessonScreen(
+                            lessonContents: content, lessonNumber: index + 1)));
+              }
+            : () {
+                AppSnackBar.show(context,
+                    message:
+                        'Oops. Lesson is not yet available. Ask your Guide for access',
+                    type: SBMessageType.info);
+              },
       ),
     );
   }

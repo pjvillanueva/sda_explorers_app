520c6551e2167dad95c20f61466b85ba45a782b1
Paul James Villanueva <77760810+pjvillanueva@users.noreply.github.com>
2025-05-07 05:33:49 +0800

    feat: added explorer detail screen

---
+++ lib/presentation/screens/Home/screens/ExplorerDetails/explorer_detail_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sda_explorers_app/data/models/explorer.dart';
+import 'package:sda_explorers_app/logic/services/helpers.dart';
+import 'package:sda_explorers_app/presentation/custom%20widgets/avatar.dart';
+import 'package:sda_explorers_app/presentation/screens/Home/screens/ExplorerDetails/components/lesson_control_grid.dart';
+import 'package:sda_explorers_app/presentation/screens/Home/screens/ExplorerDetails/cubits/explorer_detail_cubit.dart';
+
+class ExplorerDetailScreen extends StatelessWidget {
+  const ExplorerDetailScreen({super.key, required this.explorer});
+
+  final Explorer explorer;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          ExplorerDetailCubit()..initialLessonControl(explorer.allowedLessons),
+      lazy: true,
+      child: Scaffold(
+          appBar: AppBar(),
+          body: SafeArea(
+              child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        AppAvatar(
+                          size: 100.0,
+                          innerColor:
+                              colorFromHex(explorer.user?.backgroundColor),
+                          avatar: explorer.user?.avatar ?? 'matthew',
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          explorer.user?.fullName ?? 'Guest Explorer',
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  LessonControlGrid(explorerId: explorer.id)
+                ],
+              ),
+            ),
+          ))),
+    );
+  }
+}
